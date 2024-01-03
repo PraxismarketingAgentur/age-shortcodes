0be8378bcb4046d77c80e3b44467e26f4f58613d
Praxismarketing Agentur <137172862+PraxismarketingAgentur@users.noreply.github.com>
2024-01-03 11:17:04 +0100

    0.2
    * Added check if shortcode is already registered
    * Clarifying the usage of the shortcode in the readme.txt

--- age-shortcodes.php
+++ age-shortcodes.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Age Shortcodes
  * Plugin URI:        https://praxismarketing-agentur.com/wordpress-plugins/age-shortcodes/
  * Description:       Add shortcodes for the age of someone or something in years
- * Version:           0.1
+ * Version:           0.2
  * Author:            Lukas Pugstaller
  * Author URI:        https://praxismarketing-agentur.com/
  * License:           GPL v2 or later
@@ -18,13 +18,34 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function age_in_years_shortcodes( $atts ) {
 
     if ( empty( $atts['birthday'] ) )
-        return '';
+        return 0;
 
-    if ( ( $birthday = strtotime( $atts['birthday'] ) ) === false )
-        return '';
+    if ( ( strtotime( $atts['birthday'] ) ) === false )
+        return 0;
 
     $years_diff = date_diff( date_create(), date_create( $atts['birthday'] ) )->y;
-    return $years_diff;  
+
+    if ( $years_diff === false )
+        return 0;
+
+    return $years_diff;
 
 }
-add_shortcode( 'age', 'age_in_years_shortcodes' );
+
+function age_in_years_shortcodes_notice() {
+
+    echo '<div class="notice notice-error"><p>The Age Shortcodes plugin is not working because the shortcode [age] is already registered.</p></div>';
+
+}
+
+function age_in_years_shortcodes_init() {
+
+    if ( shortcode_exists( 'age' ) )
+        add_action( 'all_admin_notices', 'age_in_years_shortcodes_notice' );
+
+    else
+        add_shortcode( 'age', 'age_in_years_shortcodes' );
+
+}
+
+add_action( 'init', 'age_in_years_shortcodes_init' );
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: praxismarketing
 Tags: shortcode, age, year, current, date, time
 Requires at least: 4.0
 Tested up to: 6.2.2
-Stable tag: 0.1
+Stable tag: 0.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -13,16 +13,20 @@ Add shortcodes for the age of someone or something in years
 
 **Age Shortcodes** lets you insert the current age in years of someone or something anywhere in your WordPress site with a simple shortcode.
 
-Use the shortcode **[age birthday="dd-mm-yyyy"]** in any post, page or widget to display the current age in years.
+Use the shortcode **`[age birthday="yyyy-mm-dd"]`** in any post, page or widget to display the current age in years.
 
 ## How it works
 
-**[age birthday="20-04-1969"]** - inserts the current age in years e.g. **54**
-**[age birthday="1969-04-20"]** - inserts the current age in years e.g. **54**
-**[age birthday="20.04.1969"]** - inserts the current age in years e.g. **54**
-**[age birthday="20. April 1969"]** - inserts the current age in years e.g. **54**
+The `[age birthday="1973-10-15"]` year old American musician Dax Riggs is best known for fronting the band Acid Bath.
+
+The Golden Gate Bridge was opened `[age birthday="1937-05-27"]` years ago.
 
 == Changelog ==
+
+= 0.2 =
+
+* Added check if shortcode is already registered
+* Clarifying the usage of the shortcode in the readme.txt
 
 = 0.1 =
 
